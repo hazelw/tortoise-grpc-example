28ced03a98b60713ac0e6cd57dc097e5a19c70ed
Hazel Wright <hazel.wright@zego.com>
2021-04-07 16:59:57 +0100

    use grpc.aio.server

--- server.py
+++ server.py
@@ -1,18 +1,17 @@
-from concurrent import futures
-
+import asyncio
 import grpc
 
 import snakes_pb2_grpc
 from snakes_server import SnakesServiceServicer
 
 
-def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+async def serve():
+    server = grpc.aio.server()
     snakes_pb2_grpc.add_SnakesServiceServicer_to_server(SnakesServiceServicer(), server)
     server.add_insecure_port("[::]:50052")
-    server.start()
-    server.wait_for_termination()
+    await server.start()
+    await server.wait_for_termination()
 
 
 if __name__ == "__main__":
-    serve()
+    asyncio.run(serve())
--- snakes_server.py
+++ snakes_server.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import snakes_pb2
 import snakes_pb2_grpc
 
@@ -8,14 +10,18 @@ class SnakesServiceServicer(snakes_pb2_grpc.SnakesServiceServicer):
     def __init__(self):
         pass
 
-    def GetSnake(self, request, context):
-        snake = Snake.get(id=request.id)
+    async def GetSnake(self, request, context):
+        async def _get_snake(id: int):
+            snake = await Snake.get(id=request.id)
+            return snake
+
+        snake = await _get_snake(request.id)
 
         if not snake:
             return snakes_pb2.GetSnakeResponse(error_message="no snake")
         return snakes_pb2.GetSnakeResponse(name=snake.name, pattern=snake.pattern)
 
-    def CreateSnake(self, request, context):
+    async def CreateSnake(self, request, context):
         snake = Snake.create(name=request.name, pattern=request.pattern)
 
         return snakes_pb2.CreateSnakeResponse(id=snake.id)
