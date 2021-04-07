e08b98b01e56d1b52f53ec6a6cc81e49fdc8b23c
Hazel Wright <hazel.wright@zego.com>
2021-04-07 13:02:51 +0100

    fix returns

--- snakes_server.py
+++ snakes_server.py
@@ -3,8 +3,11 @@ import snakes_pb2_grpc
 
 
 class SnakesServiceServicer(snakes_pb2_grpc.SnakesServiceServicer):
+    def __init__(self):
+        pass
+
     def GetSnake(self, request, context):
-        return snakes_pb2_grpc.GetSnakeResponse(id=3)
+        return snakes_pb2.GetSnakeResponse(name="snakey", pattern="dots")
 
     def CreateSnake(self, request, context):
-        return snakes_pb2_grpc.CreateSnakeResponse(name="ssss", pattern="dots")
+        return snakes_pb2.CreateSnakeResponse(id=3)
