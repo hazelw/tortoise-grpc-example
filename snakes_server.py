fd7b56272a128860f0a361b73196226f07421961
Hazel Wright <hazel.wright@zego.com>
2021-04-07 16:36:25 +0100

    no async behaviour

--- snakes_server.py
+++ snakes_server.py
@@ -1,13 +1,21 @@
 import snakes_pb2
 import snakes_pb2_grpc
 
+from app.models import Snake
+
 
 class SnakesServiceServicer(snakes_pb2_grpc.SnakesServiceServicer):
     def __init__(self):
         pass
 
     def GetSnake(self, request, context):
-        return snakes_pb2.GetSnakeResponse(name="snakey", pattern="dots")
+        snake = Snake.get(id=request.id)
+
+        if not snake:
+            return snakes_pb2.GetSnakeResponse(error_message="no snake")
+        return snakes_pb2.GetSnakeResponse(name=snake.name, pattern=snake.pattern)
 
     def CreateSnake(self, request, context):
-        return snakes_pb2.CreateSnakeResponse(id=3)
+        snake = Snake.create(name=request.name, pattern=request.pattern)
+
+        return snakes_pb2.CreateSnakeResponse(id=snake.id)
